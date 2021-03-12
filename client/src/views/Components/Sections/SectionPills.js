907685c428d2e9c0887c3020579a9014b7516bcb
Aniruddhmuley2001 <aniruddhmuley2001@yahoo.com>
2021-03-12 08:56:21 +0530

    rules added

--- client/src/views/Components/Sections/SectionPills.js
+++ client/src/views/Components/Sections/SectionPills.js
@@ -58,20 +58,25 @@ export default function SectionPills() {
                     tabIcon: GavelIcon,
                     tabContent: (
                       <span>
-                        <p>
-                          Efficiently unleash cross-media information without
-                          cross-media value. Quickly maximize timely
-                          deliverables for real-time schemas.
-                        </p>
+                        <h5>For Traders</h5>
+                        <ul>
+                          <li>Traders will trade entirely on the platform provided since the progress on the platform will only be considered while choosing a winner.</li>
+                          <li>3 days in total will be provided for trading: starting from the IPO(s) declaration until the end of 3rd day.</li>
+                          <li>A total of 10000 DA coins will be provided to the traders at the starting of the contest.</li>
+                          <li>The company’s pitches will be held on Google meet (link will be provided a day before) based on which traders can trade.</li>
+                          <li>Prices of stocks will change every 30 mins so keep up with that.</li>
+                          <li>We will also be uploading documents on our social media and you can criticize their strategies in the comment section.</li>
+                        </ul>
                         <br />
-                        <p>
-                          Dramatically maintain clicks-and-mortar solutions
-                          without functional solutions. Dramatically visualize
-                          customer directed convergence without revolutionary
-                          ROI. Collaboratively administrate empowered markets
-                          via plug-and-play networks. Dynamically procrastinate
-                          B2C users after installed base benefits.
-                        </p>
+                        <h5>For Organizations</h5>
+                        <ul>
+                          <li>The company will provide its background and strategies to solve the problems which fall under their company-specific domain. How will they implement it? Basically, you will have to convince traders and judges how your company is profitable and how it is making a good profit.</li>
+                          <li>The above-stated point will require a whole pitch meeting in front of judges and all the traders who wish to join the forum. Also, the company will have to give a written document of its strategy to make it public for those who weren’t able to join the meet.</li>
+                          <li>After the first pitch, companies will again have to submit an updated document that will incorporate points about how they are willing to change certain points or improve their previously put up points. This will happen 2 days in a row after the pitch.</li>
+                          <li>We will be providing different sectors to different companies so that companies’ creativity and perception do not clash.</li>
+                          <li>The strategies should be based on the recent problems our world is facing.</li>
+                          <li>A company with the highest percentage increase in stock price from the very beginning of the event until the very last moment of the 3rd day will be the winner in the company category.</li>
+                        </ul>
                       </span>
                     )
                   },
