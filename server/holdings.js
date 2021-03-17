b5643228c6c14555a49c8e2c1196f84e983047f3
Aniruddhmuley2001 <aniruddhmuley2001@yahoo.com>
2021-03-17 22:24:01 +0530

    making it public

--- server/holdings.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const holdingSchema  = mongoose.Schema({
-    stockId: {type : mongoose.Types.ObjectId, ref:'Stock'},
-    userId : {type:mongoose.Types.ObjectId,ref:'User'},
-    quantity : Number
-},{ timestamps: { createdAt: 'created_at' } })
-module.exports = mongoose.model('Holding', holdingSchema)
